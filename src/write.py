9c392a2ae8a9d453d23606e227697bd768b94835
Ben Power <ben.power8613@gmail.com>
2025-01-23 21:08:22 -0400

    changed type annotation to str for deck params in save_decks()

--- src/write.py
+++ src/write.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 
 def save_decks(
-    deck_1: list, deck_2: list, write_path_1: str, write_path_2: str
+    deck_1: str, deck_2: str, write_path_1: str, write_path_2: str
 ) -> None:
     """Save both final decks as a .txt file in specified path"""
     p1_path = Path(write_path_1)
