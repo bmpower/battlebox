f0967c4955b27e5144bbbf74eefa504d9751a566
Ben Power <ben.power8613@gmail.com>
2025-01-26 21:11:20 -0400

    updated function description and comment

--- src/read.py
+++ src/read.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 
 def load_cards(load_path: str) -> list[str]:
-    """Load the full battlebox cards .txt file from specified path and convert it to a list"""
+    """Load the full battlebox cards .txt file from specified path and convert it to a list."""
     path = Path(load_path)
 
     # Get cards into a list.
